29a45a952c83a8fd1b34dac6575fe038fc102fdd
Rudi Visagie <rudivisagie@icloud.com>
2023-02-02 15:55:48 +0200

    Created basic files for components. Ready for branching.

---
+++ frontend_react/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './SearchBar.scss';
+
+const SearchBar = () => {
+    return (
+        <div><h1>Searchbar Here</h1></div>
+    )
+}
+
+export default SearchBar
